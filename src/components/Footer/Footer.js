fd8a3f5d4ebb3616eae2ea6a57f75093e8ae70db
Michael Froseth <info@michaelfroseth.com>
2022-12-31 15:40:51 -0600

    Fixed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,33 +1,39 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
-    return (
-        <footer className="footer">
-            <div className="container">
-                <div className="grid-container">
-                    <div className="column">
-                        <img src="assets/logo.svg" width="261px" height="268px" alt="RüMLENDR" className="featured-img" />
-                    </div>
-                    <div className="column">
-                        <ul className="menu-links">
-                            <li>
-                                <a href="#">Home</a>
-                            </li>
-                            <li>
-                                <a href="#">How To Get Involved</a>
-                            </li>
-                            <li>
-                                <a href="#">Download Pitch Deck</a>
-                            </li>
-                            <li>
-                                <a href="#">Contact Us</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="grid-container">
+          <div className="column">
+            <img
+              src="assets/logo.svg"
+              width="261px"
+              height="268px"
+              alt="RüMLENDR"
+              className="featured-img"
+            />
+          </div>
+          <div className="column">
+            <ul className="menu-links">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">How To Get Involved</a>
+              </li>
+              <li>
+                <a href="#">Download Pitch Deck</a>
+              </li>
+              <li>
+                <a href="#">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
