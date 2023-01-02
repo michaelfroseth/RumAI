029a22042a72e5455159fb34300bba5646ebe3aa
Michael Froseth <info@michaelfroseth.com>
2023-01-01 20:28:58 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from "./assets/logo.svg";
 import "./App.css";
 import "./css/style.css";
 import Header from "./components/Header/Header";
 import IntroSection from "./components/Intro/Intro";
 import Footer from "./components/Footer/Footer";
-import ParallaxBody from "./components/Parallax";
 import AnimatedCursor from "react-animated-cursor";
 
 function App() {
